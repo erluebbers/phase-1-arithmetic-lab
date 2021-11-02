7be5bb1a7520cdd2dabf7a95eed84c97ffc65ed2
Erik Luebbers <erluebbers@gmail.com>
2021-11-02 06:12:17 -0700

    commit tree 8460d230d9a0b53cd59c0bbe461737ce0f7889d1

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random()*10)
+
+const num3 = 25
+const num4 = 7
+const mod = 25 % 7
+
+const max = Math.max(18, 19, 20)
